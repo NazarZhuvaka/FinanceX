2e1c7aad5e290eee181424e0dd655588bdfb488d
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-10 19:03:48 +0300

    commit

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function navToggle() {
 	document.getElementById("headerUl").classList.toggle("_active");
 	document.getElementById("html").classList.toggle("_overflow");
 }
+
+window.onload = function() {
+    window.setInterval(function(){
+        var date = new Date();
+
+
+        var years = date.getFullYear();
+        
+
+        var clock = years;
+
+        document.getElementById("year-clock").innerHTML = clock; 
+    }
+    );
+}

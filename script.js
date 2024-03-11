dbe94c60ee859ac5c68124c4a3d3c53fdb41f4ce
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-11 13:53:36 +0300

    coomit

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: '2',
+    speed: '500',
+    
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-custom-button-next',
+      prevEl: '.swiper-custom-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+  
 function navToggle() {
 	document.getElementById("headerUl").classList.toggle("_active");
 	document.getElementById("html").classList.toggle("_overflow");
@@ -17,3 +42,4 @@ window.onload = function() {
     }
     );
 }
+

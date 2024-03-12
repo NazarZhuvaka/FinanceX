be366bab27b73f16677101d1ffc76f4174409620
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-12 15:10:39 +0300

    commit

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
 function navToggle() {
-	document.getElementById("headerUl").classList.toggle("_active");
-	document.getElementById("html").classList.toggle("_overflow");
+  document.getElementById("headerUl").classList.toggle("_active");
+  document.getElementById("html").classList.toggle("_overflow");
 }
 
-window.onload = function() {
-    window.setInterval(function(){
-        var date = new Date();
+window.onload = function () {
+  window.setInterval(function () {
+    var date = new Date();
 
+    var years = date.getFullYear();
 
-        var years = date.getFullYear();
-        
+    var clock = years;
 
-        var clock = years;
-
-        document.getElementById("year-clock").innerHTML = clock; 
-    }
-    );
-}
+    document.getElementById("year-clock").innerHTML = clock;
+  });
+};
 
 // Початкова конфігурація Swiper
 const initialSwiperConfig = {
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
   slidesPerView: 3.6,
   speed: 500,
   spaceBetween: 30,
   grabCursor: true,
-  centeredSlides: 'auto',
+  centeredSlides: "auto",
   navigation: {
-    nextEl: '.swiper-custom-button-next',
-    prevEl: '.swiper-custom-button-prev',
+    nextEl: ".swiper-custom-button-next",
+    prevEl: ".swiper-custom-button-prev",
   },
 };
 
 // Створення екземпляру Swiper з початковою конфігурацією
-const swiper = new Swiper('.swiper', initialSwiperConfig);
+const swiper = new Swiper(".swiper", initialSwiperConfig);
 
 // Функція для оновлення конфігурації Swiper при зміні розміру вікна
 function updateSwiperConfig() {
   const screenWidth = window.innerWidth;
 
-   if (screenWidth <=1920) {
+  if (screenWidth <= 1920) {
     swiper.params.slidesPerView = 3.6;
-  } 
+  }
   if (screenWidth <= 1350) {
     swiper.params.slidesPerView = 2;
-  }  if (screenWidth <=768) {
+  }
+  if (screenWidth <= 768) {
     swiper.params.slidesPerView = 1;
-  } 
-  // else {
-  //   swiper.params.slidesPerView = initialSwiperConfig.slidesPerView;
-  // }
+  }
 
-  // Оновлення конфігурації Swiper
   swiper.update();
 }
 
 // Виклик функції при завантаженні сторінки та при зміні розміру вікна
-window.addEventListener('load', updateSwiperConfig);
-window.addEventListener('resize', updateSwiperConfig);
-
+window.addEventListener("load", updateSwiperConfig);
+window.addEventListener("resize", updateSwiperConfig);
